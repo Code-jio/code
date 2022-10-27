801cbe8059dcfb445f742e7cb0914a24a5d8b73e
Code-jio <1054037036@qq.com>
2022-10-27 14:21:41 +0800

    提升打包构建速度

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,13 @@
+
+const os = require("os");
 // nodejs的核心模块，专门用来处理文件路径
 const path = require("path");
 const ESlintWebpackPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+// cpu核数
+const threads = os.cpus().length;
 
 module.exports = {
     // 入口
@@ -22,84 +28,105 @@ module.exports = {
     // 加载器
     module: {
         rules: [
-            // css
             {
-                test: /\.css$/i,
-                use: [ // 执行顺序，从右到左，从下到上
-                    "style-loader", // 将js中css通过style标签添加到html文件中生效
-                    "css-loader"  // 将css资源编译成conmonjs的模块到js中
-                ],
-            },
-            // less
-            {
-                test: /\.less$/,
-                // loader:"xxx",  // 只能使用一个loader
-                use: [ // 使用多个loader
-                    "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ]
-            },
+                // 每个文件只能被其中一个loader配置处理
+                oneOf: [
+                    // css
+                    {
+                        test: /\.css$/i,
+                        use: [ // 执行顺序，从右到左，从下到上
+                            "style-loader", // 将js中css通过style标签添加到html文件中生效
+                            "css-loader"  // 将css资源编译成conmonjs的模块到js中
+                        ],
+                    },
+                    // less
+                    {
+                        test: /\.less$/,
+                        // loader:"xxx",  // 只能使用一个loader
+                        use: [ // 使用多个loader
+                            "style-loader",
+                            "css-loader",
+                            "less-loader"
+                        ]
+                    },
 
-            // scss sass
-            {
-                test: /\.s[ac]ss$/,
-                use: [{
-                    loader: "style-loader" // 将 JS 字符串生成为 style 节点
-                }, {
-                    loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
-                }, {
-                    loader: "sass-loader" // 将 Sass 编译成 CSS
-                }]
-            },
-            // stylus
-            {
-                test: /\.styl$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "stylus-loader" // 处理styl资源
-                ],
-            },
-            // 静态文件
-            {
-                test: /\.(png|jpe?g|gif|webp|svg)$/,
-                type: "asset",
-                parser: {
-                    dataUrlCondition: {
-                        // 小于10kb的图片转化为base64
-                        // 优点：减少请求数量  缺点：体积变大约30%
-                        maxSize: 10 * 1024 // 10kb
-                    }
-                },
-                generator: {
-                    // 将图片文件输出到 static/imgs目录中
-                    // 将图片文件命名为 [hash:8][ext][query]
-                    // [hash:8]:hash值取8位
-                    // [ext]：使用之前的文件扩展名
-                    // [query]：添加之前的query参数(可写可不写)
-                    filename: "static/imgs/[hash:10][ext][query]"
-                }
-            },
-            // 字体图标
-            {
-                test: /\.(ttf|woff2?|map4|map3|avi)$/,// 处理字体图标等等资源
-                type: "asset/resource",
-                generator: {
-                    // 输出名称
-                    filename: "static/media/[hash:10][ext][query]"
-                }
-            },
-            // bable
-            {
-                test: /\.js$/,
-                exclude: /node_modules/, // 排除node_modules代码不编译
-                loader: "babel-loader",
-                // 可以在这里写预设  也可以在外面写
-                // options: {
-                //     presets: ['@babel/preset-env']
-                // }
-            },
+                    // scss sass
+                    {
+                        test: /\.s[ac]ss$/,
+                        use: [{
+                            loader: "style-loader" // 将 JS 字符串生成为 style 节点
+                        }, {
+                            loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+                        }, {
+                            loader: "sass-loader" // 将 Sass 编译成 CSS
+                        }]
+                    },
+                    // stylus
+                    {
+                        test: /\.styl$/,
+                        use: [
+                            "style-loader",
+                            "css-loader",
+                            "stylus-loader" // 处理styl资源
+                        ],
+                    },
+                    // 静态文件
+                    {
+                        test: /\.(png|jpe?g|gif|webp|svg)$/,
+                        type: "asset",
+                        parser: {
+                            dataUrlCondition: {
+                                // 小于10kb的图片转化为base64
+                                // 优点：减少请求数量  缺点：体积变大约30%
+                                maxSize: 10 * 1024 // 10kb
+                            }
+                        },
+                        generator: {
+                            // 将图片文件输出到 static/imgs目录中
+                            // 将图片文件命名为 [hash:8][ext][query]
+                            // [hash:8]:hash值取8位
+                            // [ext]：使用之前的文件扩展名
+                            // [query]：添加之前的query参数(可写可不写)
+                            filename: "static/imgs/[hash:10][ext][query]"
+                        }
+                    },
+                    // 字体图标
+                    {
+                        test: /\.(ttf|woff2?|map4|map3|avi)$/,// 处理字体图标等等资源
+                        type: "asset/resource",
+                        generator: {
+                            // 输出名称
+                            filename: "static/media/[hash:10][ext][query]"
+                        }
+                    },
+                    // bable
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/, // 排除node_modules代码不编译
+                        // loader: "babel-loader",
+                        // // 可以在这里写预设  也可以在外面写
+                        // options: {
+                        //     // presets: ['@babel/preset-env']
+                        //     cacheDirectory: true, // 开启babel编译缓存
+                        //     cacheCompression: false // 缓存文件不要压缩
+                        // }
+                        use: [
+                            {
+                                loader: "thread-loader",
+                                options: {
+                                    workers: threads, // 数量
+                                },
+                            },
+                            {
+                                loader: "babel-loader",
+                                options: {
+                                    cacheDirectory: true, // 开启babel编译缓存
+                                },
+                            },
+                        ],
+                    },
+                ]
+            }
         ],
     },
     // 插件
@@ -107,23 +134,32 @@ module.exports = {
         new ESlintWebpackPlugin({
             // 指定检查文件的根目录
             context: path.resolve(__dirname, "../src"),
+            exclude: "node_modules", // 默认值
+            cache: true, // 开启缓存
+            // 缓存目录
+            cacheLocation: path.resolve(
+                __dirname,
+                "../node_modules/.cache/.eslintcache"
+            ),
+            threads, // 开启多进程
         }),
         new HtmlWebpackPlugin({
             // 以public/index.html 为模板创建的文件
             // 新的html文件有两个特点：1.内容和源文件一致  2.自动引入打包生成的js等资源
             template: path.resolve(__dirname, "../public/index.html"),
-        })
+        }),
     ],
     // 开发服务器
     devServer: {
         host: "localhost", // 启动服务器域名
         port: "3000", // 启动服务器端口号
-        open: "true"  // 是否自动打开浏览器
+        open: true,  // 是否自动打开浏览器
+        hot: true  // 模块热替换 修改文件时，无需重新编译即可实现最新效果（实际开发可用vue-loader或者react-hot-loader替换）
     },
     // 模式
     mode: "development",//  开发模式
     // 开发模式：cheap-module-source-map
-    // 优点：打包编译速度快，只包含行映射
+    // 优点：打包编译速度快，只包含行映射(出现bug时 会提示哪一个文件哪一行出错)
     // 缺点：没有列映射
     devtool: "cheap-module-source-map",
 }
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,16 @@
+// nodejs核心模块，直接使用
+const os = require("os");
 // nodejs的核心模块，专门用来处理文件路径
 const path = require("path");
 const ESlintWebpackPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+// cpu核数
+const threads = os.cpus().length
+
 
 // 获取处理样式的Loaders
 const getStyleLoaders = (preProcessor) => {
@@ -43,68 +50,89 @@ module.exports = {
     // 加载器
     module: {
         rules: [
-            // css
             {
-                test: /\.css$/i,
-                // 执行顺序，从右到左，从下到上
-                use: getStyleLoaders(),
-            },
-            // less
-            {
-                test: /\.less$/,
-                // loader:"xxx",  // 只能使用一个loader
-                use: getStyleLoaders("less-loader"),
-            },
+                // 每个文件只能被其中一个loader配置处理
+                oneOf: [
+                    // css
+                    {
+                        test: /\.css$/i,
+                        // 执行顺序，从右到左，从下到上
+                        use: getStyleLoaders(),
+                    },
+                    // less
+                    {
+                        test: /\.less$/,
+                        // loader:"xxx",  // 只能使用一个loader
+                        use: getStyleLoaders("less-loader"),
+                    },
 
-            // scss sass
-            {
-                test: /\.s[ac]ss$/,
-                use: getStyleLoaders("sass-loader"),
-            },
-            // stylus
-            {
-                test: /\.styl$/,
-                use: getStyleLoaders("stylus-loader"),
-            },
-            // 静态文件
-            {
-                test: /\.(png|jpe?g|gif|webp|svg)$/,
-                type: "asset",
-                parser: {
-                    dataUrlCondition: {
-                        // 小于10kb的图片转化为base64
-                        // 优点：减少请求数量  缺点：体积变大约30%
-                        maxSize: 10 * 1024 // 10kb
-                    }
-                },
-                generator: {
-                    // 将图片文件输出到 static/imgs目录中
-                    // 将图片文件命名为 [hash:8][ext][query]
-                    // [hash:8]:hash值取8位
-                    // [ext]：使用之前的文件扩展名
-                    // [query]：添加之前的query参数(可写可不写)
-                    filename: "static/imgs/[hash:10][ext][query]"
-                }
-            },
-            // 字体图标
-            {
-                test: /\.(ttf|woff2?|map4|map3|avi)$/,// 处理字体图标等等资源
-                type: "asset/resource",
-                generator: {
-                    // 输出名称
-                    filename: "static/media/[hash:10][ext][query]"
-                }
-            },
-            // bable
-            {
-                test: /\.js$/,
-                exclude: /node_modules/, // 排除node_modules代码不编译
-                loader: "babel-loader",
-                // 可以在这里写预设  也可以在外面写
-                // options: {
-                //     presets: ['@babel/preset-env']
-                // }
-            },
+                    // scss sass
+                    {
+                        test: /\.s[ac]ss$/,
+                        use: getStyleLoaders("sass-loader"),
+                    },
+                    // stylus
+                    {
+                        test: /\.styl$/,
+                        use: getStyleLoaders("stylus-loader"),
+                    },
+                    // 静态文件
+                    {
+                        test: /\.(png|jpe?g|gif|webp|svg)$/,
+                        type: "asset",
+                        parser: {
+                            dataUrlCondition: {
+                                // 小于10kb的图片转化为base64
+                                // 优点：减少请求数量  缺点：体积变大约30%
+                                maxSize: 10 * 1024 // 10kb
+                            }
+                        },
+                        generator: {
+                            // 将图片文件输出到 static/imgs目录中
+                            // 将图片文件命名为 [hash:8][ext][query]
+                            // [hash:8]:hash值取8位
+                            // [ext]：使用之前的文件扩展名
+                            // [query]：添加之前的query参数(可写可不写)
+                            filename: "static/imgs/[hash:10][ext][query]"
+                        }
+                    },
+                    // 字体图标
+                    {
+                        test: /\.(ttf|woff2?|map4|map3|avi)$/,// 处理字体图标等等资源
+                        type: "asset/resource",
+                        generator: {
+                            // 输出名称
+                            filename: "static/media/[hash:10][ext][query]"
+                        }
+                    },
+                    // bable
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/, // 排除node_modules代码不编译
+                        // loader: "babel-loader",
+                        // 可以在这里写预设  也可以在外面写
+                        // options: {
+                        //     // presets: ['@babel/preset-env']
+                        //     cacheDirectory: true, // 开启babel编译缓存
+                        //     cacheCompression: false // 缓存文件不要压缩
+                        // }
+                        use: [
+                            {
+                                loader: "thread-loader",
+                                options: {
+                                    workers: threads, // 数量
+                                },
+                            },
+                            {
+                                loader: "babel-loader",
+                                options: {
+                                    cacheDirectory: true, // 开启babel编译缓存
+                                },
+                            },
+                        ],
+                    },
+                ]
+            }
         ],
     },
     // 插件
@@ -112,6 +140,14 @@ module.exports = {
         new ESlintWebpackPlugin({
             // 指定检查文件的根目录
             context: path.resolve(__dirname, "../src"),
+            exclude: "node_modules", // 默认值
+            cache: true, // 开启缓存
+            // 缓存目录
+            cacheLocation: path.resolve(
+                __dirname,
+                "../node_modules/.cache/.eslintcache"
+            ),
+            threads, // 开启多进程
         }),
         new HtmlWebpackPlugin({
             // 以public/index.html 为模板创建的文件
@@ -124,8 +160,20 @@ module.exports = {
             filename: "static/css/main.css"
         }),
         // css压缩
-        new CssMinimizerPlugin(),
+        // new CssMinimizerPlugin(),
+        
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // css压缩也可以写到optimization.minimizer里面，效果一样的
+            new CssMinimizerPlugin(),
+            // 当生产模式会默认开启TerserPlugin，但是我们需要进行其他配置，就要重新写了
+            new TerserPlugin({
+                parallel: threads, // 开启多线程
+            })
+        ]
+    },
     // 开发服务器
     // devServer: {
     //     host: "localhost", // 启动服务器域名
